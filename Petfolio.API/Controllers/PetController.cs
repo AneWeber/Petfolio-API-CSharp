051f1a31b01aeb4e12994eb1da3a11d5111c9607
Ane Marinheiro Weber <anemariweber@gmail.com>
2025-07-18 16:44:16 +0200

    feat: GetAll UseCase

--- Petfolio.API/Controllers/PetController.cs
+++ Petfolio.API/Controllers/PetController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Petfolio.Application.UseCases.Pet.GetAll;
 using Petfolio.Application.UseCases.Pet.Register;
 using Petfolio.Application.UseCases.Pet.Update;
 using Petfolio.Communication.Requests;
@@ -28,6 +29,24 @@ public class PetController : ControllerBase
         var useCase = new UpdatePetUseCase();
 
         useCase.Execute(id, request);
+
+        return NoContent();
+    }
+
+    [HttpGet]
+    [ProducesResponseType(typeof(ResponseAllPetsJson), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(typeof(ResponseErrorJson), StatusCodes.Status400BadRequest)]
+    public IActionResult GetAll()
+    {
+        var useCase = new GetAllPetsUseCase();
+
+        var response = useCase.Execute();
+
+        if(response.Pets.Any())
+        {
+            return Ok(response);
+        }
 
         return NoContent();
     }
